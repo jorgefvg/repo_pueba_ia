90bdb294dd1212a77fe6c99a4d4425923e003a00
Jorge Vasquez <jorgefernandov10@gmail.com>
2026-03-02 22:38:42 -0600

    Se agrega archivo doxyfile

--- src/calculadora.py
+++ src/calculadora.py
@@ -1,6 +1,26 @@
-def sumar(a, b):
+"""
+Modulo para implmentacion de una calculadora.
+
+### Registro de cambios
+| REV | YYYY.MM.DD | Autor           | Descripción de los cambios                              |
+|-----|------------|-----------------|---------------------------------------------------------|
+|   1 | 2025.11.14 | Jorge Vásquez   | Versión inicial del archivo                             |
+
+Este paquete proveee de funciones basicas de suma y resta.
+"""
+
+
+## Funcion sumar
+# @param a Primer numero a sumar
+# @param b Segundo numero a sumar
+# @return int
+def sumar(a: int, b: int) -> int:
     return a + b
 
 
-def restar(a, b):
+## Funcion restar
+# @param a Primer numero a restar
+# @param b Segundo numero a restar
+# @return int
+def restar(a: int, b: int) -> int:
     return a - b
